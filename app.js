6ecec5eeb5d4881a20cccd80c591151e13f30682
Sarthak <sarthakn18@gmail.com>
2017-06-30 10:39:03 +0530

    Added choose houses feature

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ var express     = require('express');
 var app         = express();
 var http        = require('http').Server(app);
 var path        = require('path');
-
+var housesJSON  = require('./houses.json');
 
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',function(req,res){
-  res.render('index');
+  res.render('index',{
+    title: 'Welcome'
+  });
 });
 
 app.get('/chatroom',function(req,res){
-  res.render('chatroom');
+  res.render('chatroom',{
+    title:'Chat Menu'
+  });
 });
+
+app.get('/choose',function(req,res){
+    var houseArr = housesJSON.house;
+    res.render('choose',{
+      title: 'Choose Your house',
+      houseArr: houseArr
+    })
+})
 
 
 
